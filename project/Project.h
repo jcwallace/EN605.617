011f911c072bb628d1e20010a7a835922783b002
Jacob Wallace <jacobcw@sbcglobal.net>
2022-05-05 18:21:31 -0400

    Setup declarations

--- project/Project.h
+++ project/Project.h
@@ -1,27 +1,56 @@
-__global__
-void add(int * arr1, int * arr2, int * out)
-{    
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    out[thread_idx] = arr1[thread_idx] + arr2[thread_idx];
-}
+// Common Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <cuda.h>
 
-__global__  
-void sub(int * arr1, int * arr2, int * out){
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    out[thread_idx] = arr1[thread_idx] - arr2[thread_idx];
-}
 
-__global__
-void mult(int * arr1, int * arr2, int * out){
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    out[thread_idx] = arr1[thread_idx] * arr2[thread_idx];
-}
-    
-__global__ 
-void mod(int * arr1, int * arr2, int * out){
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    out[thread_idx] = arr1[thread_idx] % arr2[thread_idx];
-}
+// Constants
+#define NUM_RUNS 4
+
+// Squared matrices are tested within a certain range (e.g. 1024x1024, 2048x2048, 4096x4096)
+#define MINSIZE (1024)
+#define MAXSIZE (4*1024)
+
+// Set the alpha and beta values for the cuBLAS and clBlas libraries. Note that the myGEMM kernels
+// for simplicity only support alpha values of 1 and beta values of 0.
+#define ALPHA 1.0f
+#define BETA 0.0f
+
+#define GPU_NAME "Tesla K80"
+#define GPU_CLOCK 0.823 // Core clock in GHz
+#define GPU_CORES 4992 // Total number of CUDA cores
+//#define GPU_MOD 2 // Fused multiply-add
+
+typedef struct{
+	double t;
+	int long long kflops;
+} profiler;
+
+// Number of timers
+#define NUM_TIMERS 10
+
+extern profiler timers[NUM_TIMERS];
+
+double timer(void);
+double wtime(profiler timer);
+double gflops(profiler timer);
+
+void libcublas(float* A, float* B, float* C,
+               int K, int M, int N,
+               int timerID);
+
+void libclblas(float* A, float* B, float* C,
+               int K, int M, int N,
+               int timerID);
+
+void mycublas(float* A, float* B, float* C,
+              int K, int M, int N,
+              int timerID);
+
+void myclblas(float* A, float* B, float* C,
+              int K, int M, int N,
+              int timerID);
 
 class CUDAMatMult {
 
--- project/project.cu
+++ project/project.cu
@@ -1,159 +1,52 @@
-//Based on the work of Andrew Krepps
-#include <stdio.h>
-#include <stdlib.h>
-#include <chrono>
-    
-#define NUM_ELEMENTS 100000    
+#include "project.h"    
 
-__global__
-void add(int * arr1, int * arr2, int * out)
-{    
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    out[thread_idx] = arr1[thread_idx] + arr2[thread_idx];
-}
-    
-
-__global__  
-void sub(int * arr1, int * arr2, int * out){
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    out[thread_idx] = arr1[thread_idx] - arr2[thread_idx];
-}
-    
-
-__global__
-void mult(int * arr1, int * arr2, int * out){
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    out[thread_idx] = arr1[thread_idx] * arr2[thread_idx];
-}
-    
-
-__global__ 
-void mod(int * arr1, int * arr2, int * out){
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    out[thread_idx] = arr1[thread_idx] % arr2[thread_idx];
-}
-
-       
-void main_math(int blockSize, int numBlocks) {
-    
-    // Main Assignment Function calls
-    int array1[NUM_ELEMENTS];
-    int array2[NUM_ELEMENTS];
-    int output[NUM_ELEMENTS];
-    
-    for( int i = 0; i<NUM_ELEMENTS; i++){
-        array1[i] = i;
-        array2[i] = rand() % 4;
-    }
-    
-    int *arr1,*arr2,*out;
-    
-    int arraySize_bytes = (sizeof(unsigned int) * NUM_ELEMENTS);                             
-    cudaMalloc((void **)&arr1, arraySize_bytes);
-    cudaMalloc((void **)&arr2, arraySize_bytes);
-    cudaMalloc((void **)&out, arraySize_bytes);
-                   
-    auto start = std::chrono::high_resolution_clock::now();                                
-    cudaMemcpy(arr1,array1,arraySize_bytes,cudaMemcpyHostToDevice);                                
-    cudaMemcpy(arr2,array2,arraySize_bytes,cudaMemcpyHostToDevice);                                 
-                                 
-    add<<<numBlocks,blockSize>>>(arr1,arr2,out);
-    sub<<<numBlocks,blockSize>>>(arr1,arr2,out);
-    mult<<<numBlocks,blockSize>>>(arr1,arr2,out);
-    mod<<<numBlocks,blockSize>>>(arr1,arr2,out);
-    
-    cudaMemcpy(output,out,arraySize_bytes,cudaMemcpyDeviceToHost);
-    
-    auto stop = std::chrono::high_resolution_clock::now(); 
-    float delta = std::chrono::duration<double,std::milli>(stop - start).count();
-    printf("-- Global memory (Baseline) [%f ms]\n", delta);
-    
-    cudaFree(arr1);
-    cudaFree(arr2);
-    cudaFree(out);
-    
-}
-
- 
-void main_math_stream(int blockSize, int numBlocks){
-    
-    int arraySize_bytes = (sizeof(unsigned int) * NUM_ELEMENTS);
-    int *host_arr1, *host_arr2, *host_out;
-    int *dev_arr1, *dev_arr2, *dev_out;
-                                    
-    cudaStream_t stream; 
-    cudaStreamCreate(&stream);
-                                                                    
-    cudaMalloc((void **)&dev_arr1, arraySize_bytes);
-    cudaMalloc((void **)&dev_arr2, arraySize_bytes);
-    cudaMalloc((void **)&dev_out, arraySize_bytes);
-    
-    cudaHostAlloc((void **)&host_arr1, arraySize_bytes, cudaHostAllocDefault);
-    cudaHostAlloc((void **)&host_arr2, arraySize_bytes, cudaHostAllocDefault);
-    cudaHostAlloc((void **)&host_out, arraySize_bytes, cudaHostAllocDefault);
-    
-    for( int i = 0; i<NUM_ELEMENTS; i++){
-        host_arr1[i] = i;
-        host_arr2[i] = rand() % 4;
-    }
-        
-    auto start = std::chrono::high_resolution_clock::now();                                
-                                    
-    cudaMemcpyAsync(dev_arr1, host_arr1, arraySize_bytes, cudaMemcpyHostToDevice, stream);
-    cudaMemcpyAsync(dev_arr2, host_arr2, arraySize_bytes, cudaMemcpyHostToDevice, stream);                                
-                                    
-    add<<<NUM_ELEMENTS,numBlocks,blockSize,stream>>>(dev_arr1,dev_arr2,dev_out);
-    sub<<<NUM_ELEMENTS,numBlocks,blockSize,stream>>>(dev_arr1,dev_arr2,dev_out);
-    mult<<<NUM_ELEMENTS,numBlocks,blockSize,stream>>>(dev_arr1,dev_arr2,dev_out);
-    mod<<<NUM_ELEMENTS,numBlocks,blockSize,stream>>>(dev_arr1,dev_arr2,dev_out);
-    
-    cudaMemcpyAsync(host_out, dev_out, arraySize_bytes, cudaMemcpyDeviceToHost, stream);
-    cudaStreamSynchronize(stream);
-    
-    auto stop = std::chrono::high_resolution_clock::now(); 
-    float delta = std::chrono::duration<double,std::milli>(stop - start).count();
-    printf("-- Stream and Event [%f ms]\n", delta);
-    
-    cudaFreeHost(host_arr1);
-    cudaFreeHost(host_arr2);
-    cudaFreeHost(host_out);
-    cudaFree(dev_arr1);
-    cudaFree(dev_arr2);
-    cudaFree(dev_out);
-    cudaDeviceReset();
+// Print device properties
+void printDevProp(cudaDeviceProp devProp)
+{
+    printf("Major revision number:         %d\n",  devProp.major);
+    printf("Minor revision number:         %d\n",  devProp.minor);
+    printf("Name:                          %s\n",  devProp.name);
+    printf("Total global memory:           %u\n",  devProp.totalGlobalMem);
+    printf("Total shared memory per block: %u\n",  devProp.sharedMemPerBlock);
+    printf("Total registers per block:     %d\n",  devProp.regsPerBlock);
+    printf("Warp size:                     %d\n",  devProp.warpSize);
+    printf("Maximum memory pitch:          %u\n",  devProp.memPitch);
+    printf("Maximum threads per block:     %d\n",  devProp.maxThreadsPerBlock);
+    for (int i = 0; i < 3; ++i)
+    printf("Maximum dimension %d of block:  %d\n", i, devProp.maxThreadsDim[i]);
+    for (int i = 0; i < 3; ++i)
+    printf("Maximum dimension %d of grid:   %d\n", i, devProp.maxGridSize[i]);
+    printf("Clock rate:                    %d\n",  devProp.clockRate);
+    printf("Total constant memory:         %u\n",  devProp.totalConstMem);
+    printf("Texture alignment:             %u\n",  devProp.textureAlignment);
+    printf("Concurrent copy and execution: %s\n",  (devProp.deviceOverlap ? "Yes" : "No"));
+    printf("Number of multiprocessors:     %d\n",  devProp.multiProcessorCount);
+    printf("Kernel execution timeout:      %s\n",  (devProp.kernelExecTimeoutEnabled ? "Yes" : "No"));
+    return;
 }
     
     
 int main(int argc, char** argv)
 {
-	// read command line arguments
-	int totalThreads = 256;
-	int blockSize = 256;
-    int numBlocks = totalThreads/blockSize;
-    	
-	if (argc >= 2) {
-		totalThreads = atoi(argv[1]);
-	}
-	if (argc >= 3) {
-		blockSize = atoi(argv[2]);
-	}
+	// Number of CUDA devices
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    printf("CUDA Device Query...\n");
+    printf("There are %d CUDA devices.\n", devCount);
 
-	// validate command line arguments
-	if (totalThreads % blockSize != 0) {
-		++numBlocks;
-		totalThreads = numBlocks*blockSize;
-		
-		printf("Warning: Total thread count is not evenly divisible by the block size\n");
-		printf("The total number of threads will be rounded up to %d\n", totalThreads);
-	}
-    
-    numBlocks = totalThreads/blockSize;
-    
-    printf("Thread Count: %d\n",totalThreads);
-    printf("Block Size: %d\n",blockSize);
-    printf("Array Size: %d\n",NUM_ELEMENTS);
-    
-    main_math(blockSize, numBlocks);
-    main_math_stream(blockSize, numBlocks);
-    
+    // Iterate through devices
+    for (int i = 0; i < devCount; ++i)
+    {
+        // Get device properties
+        printf("\nCUDA Device #%d\n", i);
+        cudaDeviceProp devProp;
+        cudaGetDeviceProperties(&devProp, i);
+        printDevProp(devProp);
+    }
+
+    printf("\nPress any key to exit...");
+    char c;
+    scanf("%c", &c);
+
+    return 0;
 }
